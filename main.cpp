e8db40a44fd63e4187ebadca7c926b59fa2013b4
iammehtabsahir <mehtabrajputofficial@gmail.com>
2025-04-13 12:13:02 +0500

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,40 @@
 #include <iostream>
-#include "calculator.h" // Include the header file
-
+#include "calculator.h"
+using namespace std; 
 int main() {
     Calculator calc;
-    int num1 = 10;
-    int num2 = 5;
+    int num1, num2;
+    char operation;
 
-    std::cout << "Addition: " << calc.add(num1, num2) << std::endl;
-    std::cout << "Subtraction: " << calc.subtract(num1, num2) << std::endl;
+    cout << "Enter the first number: ";
+    cin >> num1;
+
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    cout << "Enter an operation (+, -, *, /): ";
+    cin >> operation;
+
+    switch (operation) {
+        case '+':
+            cout << "Result: " << calc.add(num1, num2) << endl;
+            break;
+        case '-':
+            cout << "Result: " << calc.subtract(num1, num2) << endl;
+            break;
+        case '*':
+            cout << "Result: " << calc.multiply(num1, num2) << endl;
+            break;
+        case '/':
+            if (num2 != 0) {
+                cout << "Result: " << calc.divide(num1, num2) << endl;
+            } else {
+                cout << "Error: Cannot divide by zero!" << endl;
+            }
+            break;
+        default:
+            cout << "Invalid operation!" << endl;
+    }
 
     return 0;
 }
