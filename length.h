4aef94a49df853d444adcaaf66d61a48d86952a1
ASHFAQUE ALI CHANNA <channaashfaq34@gmail.com>
2025-04-13 14:27:39 +0500

    length.h

---
+++ length.h
@@ -0,0 +1,12 @@
+#ifndef LENGTH_H
+#define LENGTH_H
+
+class LengthConverter {
+public:
+    double metersToFeet(double meters);
+    double feetToMeters(double feet);
+    double kilometersToMiles(double kilometers);
+    double milesToKilometers(double miles);
+};
+
+#endif // LENGTH_H
