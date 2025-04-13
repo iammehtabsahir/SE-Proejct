2be0eca1c0241b18d7d4442623640f3871d24b00
ASHFAQUE ALI CHANNA <channaashfaq34@gmail.com>
2025-04-13 14:31:30 +0500

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "calculator.h"
-#include "temperature.h" // Include the temperature header
+#include "temperature.h"
+#include "length.h" // Include the length header
 
 using namespace std;
 
@@ -10,7 +11,8 @@ int main() {
     cout << "Choose an option:" << endl;
     cout << "1. Use Calculator" << endl;
     cout << "2. Use Temperature Converter" << endl;
-    cout << "Enter your choice (1 or 2): ";
+    cout << "3. Use Length Converter" << endl;
+    cout << "Enter your choice (1, 2, or 3): ";
     cin >> choice;
 
     if (choice == 1) {
@@ -70,6 +72,39 @@ int main() {
             cout << "Celsius: " << tempConverter.fahrenheitToCelsius(temperature) << endl;
         } else {
             cout << "Invalid temperature conversion choice!" << endl;
+        }
+    } else if (choice == 3) {
+        LengthConverter lengthConverter;
+        int lengthChoice;
+        double length;
+
+        cout << "\nLength Converter Mode:" << endl;
+        cout << "Choose conversion:" << endl;
+        cout << "1. Meters to Feet" << endl;
+        cout << "2. Feet to Meters" << endl;
+        cout << "3. Kilometers to Miles" << endl;
+        cout << "4. Miles to Kilometers" << endl;
+        cout << "Enter your choice (1, 2, 3, or 4): ";
+        cin >> lengthChoice;
+
+        if (lengthChoice == 1) {
+            cout << "Enter length in Meters: ";
+            cin >> length;
+            cout << "Feet: " << lengthConverter.metersToFeet(length) << endl;
+        } else if (lengthChoice == 2) {
+            cout << "Enter length in Feet: ";
+            cin >> length;
+            cout << "Meters: " << lengthConverter.feetToMeters(length) << endl;
+        } else if (lengthChoice == 3) {
+            cout << "Enter length in Kilometers: ";
+            cin >> length;
+            cout << "Miles: " << lengthConverter.kilometersToMiles(length) << endl;
+        } else if (lengthChoice == 4) {
+            cout << "Enter length in Miles: ";
+            cin >> length;
+            cout << "Kilometers: " << lengthConverter.milesToKilometers(length) << endl;
+        } else {
+            cout << "Invalid length conversion choice!" << endl;
         }
     } else {
         cout << "Invalid choice!" << endl;
