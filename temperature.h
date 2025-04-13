c6d9d6029ec51cfaf28b7717f2b7ba8ce528e484
Ahmed56820 <sahabmemonsardar@gmail.com>
2025-04-13 13:38:34 +0500

    Create temperature.h

---
+++ temperature.h
@@ -0,0 +1,10 @@
+#ifndef TEMPERATURE_H
+#define TEMPERATURE_H
+
+class TemperatureConverter {
+public:
+    double celsiusToFahrenheit(double celsius);
+    double fahrenheitToCelsius(double fahrenheit);
+};
+
+#endif // TEMPERATURE_H
